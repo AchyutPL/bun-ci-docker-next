396a3e7e216455323402b793d6b095b44fb8d0b4
Achyut Paudel <achyut.paudel@biworldwide.com>
2024-04-24 17:16:42 +0545

    added cognito

--- scripts/main_script.sh
+++ scripts/main_script.sh
@@ -1,7 +1,20 @@
+# Define the folder where your server code is located
 SERVER_FOLDER=/var/www/html/bun-ci-docker-next
 
+# Change directory to the server folder or exit if it fails
 cd ${SERVER_FOLDER} || exit
 
+# Fetch all changes from the remote repository
 sudo git fetch --all
 
+# Pull the latest changes from the remote repository
 sudo git pull
+
+# Rebuild the Docker image with the latest changes
+# Make sure your Docker image name matches your Dockerfile
+# Assuming your Dockerfile is in the current directory
+sudo docker build -t nextjs_prod .
+
+# Restart the Docker container with the updated image
+# Make sure your Docker container name matches "nextjs_prod"
+sudo docker restart nextjs_prod
