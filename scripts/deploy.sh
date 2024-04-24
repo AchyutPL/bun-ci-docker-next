0f6cd2d4da660168a99a3bc547ddd3966fb24ae7
Achyut Paudel <achyut.paudel@biworldwide.com>
2024-04-24 15:33:28 +0545

    feat: added docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,5 @@
-docker restart nextjs_prod
-
 DEPLOY_SERVER="35.154.15.237"
 
 echo "Deploying to ${DEPLOY_SERVER}"
-ssh root@${DEPLOY_SERVER} 'bash -s' < ./scripts/entry.sh
-
+ssh root@${DEPLOY_SERVER} 'bash -s' < ./scripts/main_script.sh
 echo "Finished"
--- scripts/main_script.sh
+++ scripts/main_script.sh
@@ -2,10 +2,8 @@ SERVER_FOLDER=/var/www/html/bun-ci-docker-next
 
 cd ${SERVER_FOLDER} || exit
 
-source ~/.bashrc
+pwd 
 
-pwd
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
 git fetch --all
+
 git pull
