c37f1029d4dbc8b5e64986dfdd03880dae72c1b1
Achyut Paudel <achyut.paudel@biworldwide.com>
2024-04-24 16:58:05 +0545

    feat: added docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 DEPLOY_SERVER="35.154.15.237"
 
 echo "Deploying to ${SSH_PRIVATE_KEY}"
-ssh -i ./key.pem root@${DEPLOY_SERVER} 'bash -s' < ./scripts/main_script.sh
+ssh -i ./key.pem ubuntu@${DEPLOY_SERVER} 'bash -s' < ./scripts/main_script.sh
 echo "Finished"
