d3032c8572a312c19a99e071ab0d447ad6396738
Achyut Paudel <achyut.paudel@biworldwide.com>
2024-04-27 13:41:39 +0545

    added compression

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Welcome to app
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-5xl">
+        Welcome to The NEXT_BUN_DOCKER_CIRCLECI APP
     </main>
   );
 }
